087314d5135c4beb0876cdc1322f1708bb014de1
Maniktyagis <maniktyagi06@gmail.com>
2025-09-15 00:51:13 +0530

    Update building.groovy

--- vars/building.groovy
+++ vars/building.groovy
@@ -1,6 +1,6 @@
 def call(String imageName,String imageTag){
   echo "This is building stage...."
   sh "whoami"
-  sh "docker build -t ${imageName}":"${imageTag} ."
+  sh "docker build -t ${imageName}:${imageTag} ."
   echo "Building done!"
 }
